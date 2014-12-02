39bf4b041a5492e4d88b440688976d04d66b4d64
Rayno <rmostert@me.com>
2014-12-02 14:52:01 +0200

    Initial Commit

---
+++ Wingle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wingle
+//
+//  Created by Rayno Mostert on 2014/12/02.
+//  Copyright (c) 2014 Rayno Mostert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
